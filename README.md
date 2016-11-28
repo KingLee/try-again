12598e8ee68049ab94d79e55dfcaeaf94d355f66
KingLee <kingleevw@gmail.com>
2016-11-28 08:54:59 -0500

    The Rest

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# try-again
+# tome
+
+HTML/CSS/JS Boilerplate
+
+## CSS Conventions
+
+tome's CSS follows a convention that I call PCCM. Which stands for:
+
+* Prefix
+* Component
+* Child
+* Modifier
+
+Take, for instance, this CSS class:
+
+> js-profileBanner_header--transparent
+
+In this case:
+
+* "js" is the prefix
+* "profileBanner" is the component
+* "header" is the child
+* "transparent" is the modifier
+
+### Prefix
+
+### Component
+
+### Child
+
+### Modifier
---
+++ js/functionality.js
@@ -0,0 +1,75 @@
+/********************
+ ** Global Variables
+ ********************/
+
+var mq = jQuery('#js-mediaQueryReference')[0],
+    mqSupport = Modernizr.mq('only all'),
+    prevDevice = '',
+    device = '',
+    activeClass = 'is-active',
+    stickyClass = 'is-sticky',
+    staticClass = 'is-static';
+
+/********************
+ ** Custom Functions
+ ********************/
+
+ function checkDevice()
+ {
+    //Set up event listeners tied to media queries
+    mq.addEventListener('webkitTransitionEnd', checkMediaQuery, true);
+    mq.addEventListener('MSTransitionEnd', checkMediaQuery, true);
+    mq.addEventListener('oTransitionEnd', checkMediaQuery, true);
+    mq.addEventListener('transitionend', checkMediaQuery, true);
+ }
+
+//Create an event checker function that grabs the current value of the after pseudo class of the #js-mediaquery-reference <div>
+//Based on: http://seesparkbox.com/demos/css-content-check/index.html
+function checkMediaQuery()
+{
+    if (mqSupport)
+    {
+        prevDevice = device;
+        device = window.getComputedStyle(mq, ':after').getPropertyValue('content').replace(/"/g, '');
+        resetStyles();
+    }
+    else
+    {
+        device = 'desktop';
+    }
+}
+
+//Function to reset any styles that may have been changed on screen resize
+function resetStyles()
+{
+    //
+}
+
+/********************
+ ** Event Listeners
+ ********************/
+
+//Any kind of on('click') or on('change') type functions would go here
+
+/********************
+ ** jQuery Window Load
+ ********************/
+
+jQuery(window).load(function ()
+{
+    //
+});
+
+/********************
+ ** jQuery DOM Ready
+ ********************/
+
+jQuery(document).ready(function ()
+{
+    //If the browser supports media queries
+    if (mqSupport)
+    {
+        //Check what device we're on
+        checkDevice();
+    }
+});
